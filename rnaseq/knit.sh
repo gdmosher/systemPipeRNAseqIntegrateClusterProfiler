ddee0d7bc3c1c421a920def07977ec0320c53c14
Gordon David Mosher <gmosh001@ucr.edu>
2018-06-19 15:49:18 -0700

    last commit before alldata branch and added GSE4NGS_Slidy

--- rnaseq/knit.sh
+++ rnaseq/knit.sh
@@ -1,2 +1,2 @@
 cat knit.sh
-srun --x11 --partition=intel --mem=4gb --cpus-per-task 1 --ntasks 1 --time 00-2:00:00 --job-name=Knitr time Rscript -e "rmarkdown::render('systemPipeRNAseqIntegrateClusterProfiler.Rmd')"
+srun --x11 --partition=intel --mem=4gb --cpus-per-task 1 --ntasks 1 --time 07-0:00:00 --job-name=Knitr time Rscript -e "rmarkdown::render('systemPipeRNAseqIntegrateClusterProfiler.Rmd')"
---
+++ rnaseq/setLinks.sh
@@ -0,0 +1,4 @@
+rm data
+rm results
+ln -s $1data data
+ln -s $1results results
